1427fdef1137040af9f6256d1ff48509916a6bb7
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-05-07 17:17:21 -0300

    chore: order module se controller

--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { OrderService } from './order.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { OrderController } from './order.controller';
+import { OrderService } from './order.service';
+import Order from './model/order.model';
+import OrderItem from './model/order-item.model';
+import Product from 'src/product/model/product.model';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Order, OrderItem, Product])],
+  controllers: [OrderController],
   providers: [OrderService],
-  controllers: [OrderController]
+  exports: [OrderService],
 })
 export class OrderModule {}
