02e7a244cb810cdb4d54272d0e8c9f6c1af8a4ca
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2025-05-07 16:33:06 -0300

    chore; database module

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql',
+        host: configService.get('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 3306),
+        username: configService.get('DATABASE_USER', 'nestjs_user'),
+        password: configService.get('DATABASE_PASSWORD', 'nestjs_password'),
+        database: configService.get('DATABASE_NAME', 'nestjs_app'),
+        models: [__dirname + '/../**/*.model.{ts,js}'],
+        autoLoadModels: true,
+        synchronize: true,
+        logging: configService.get('NODE_ENV') !== 'production',
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
